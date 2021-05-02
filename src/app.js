67e81914df12b7bffbb0c1afb1a480924ce0f8b3
gomesconsultor <eduardocgpereira@hotmail.com>
2021-05-02 13:22:50 -0300

    correção versão

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const metricsMiddleware = promBundle({
         includePath: true, 
         customLabels: 
             { 
-                project_version: '1.2' 
+                project_version: '2.0' 
             } 
     });
 
